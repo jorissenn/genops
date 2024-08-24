ddc8f68ef63987e40fadf71c102ab39ac2cb6ca7
jorissenn <sennjoris@gmail.com>
2024-08-24 15:41:44 +0200

    implemented AlexNet CNN architecture

--- models/raster/cnn.py
+++ models/raster/cnn.py
@@ -1,3 +1,5 @@
+# Code from https://github.com/pytorch/vision/blob/main/torchvision/models/alexnet.py
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -5,24 +7,33 @@ import torch.nn.functional as F
 from classification_head_raster import ClassificationHeadRaster
 
 class CNN(nn.Module):
-    def __init__(self, n_channels, n_classes):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=n_channels, out_channels=32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+    def __init__(self, n_channels, n_classes) -> None:
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(n_channels, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
 
         # appending a classification head for each class to be predicted
-        self.classification_heads = nn.ModuleList([ClassificationHeadRaster(n_input_features=128*32*32, n_classes=1) for _ in range(n_classes)])
+        self.classification_heads = nn.ModuleList([ClassificationHeadRaster(n_input_features=256*7*7, n_classes=1) for _ in range(n_classes)])
 
-    def forward(self, x):
-        # input dimension = 256
-        x = self.pool(F.relu(self.conv1(x)))
-        # input dimension = 128
-        x = self.pool(F.relu(self.conv2(x)))
-        # input dimension = 64
-        x = self.pool(F.relu(self.conv3(x)))
-        # input dimension = 32
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
 
         if isinstance(self.classification_heads, nn.ModuleList):
             # apply each classification head and concatenate the results along the final dimension
@@ -36,4 +47,5 @@ class CNN(nn.Module):
         return n_parameters
 
     def __str__(self):
-        return f"CNN with {self.get_n_parameters():,} parameters" 
+        return f"CNN with {self.get_n_parameters():,} parameters"
+        
