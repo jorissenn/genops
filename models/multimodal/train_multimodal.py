ba2541f8e3da58882b2d3eda50c0e07ef676e826
jorissenn <sennjoris@gmail.com>
2024-09-15 23:36:19 +0200

    refactoring training loops

---
+++ models/multimodal/train_multimodal.py
@@ -0,0 +1,90 @@
+import math
+import time
+import torch
+from torch.utils.data import DataLoader
+from dataset_multimodal import collate_raster_vector
+
+def train_multimodal_model(model, device, optimizer, criterion, n_epochs, batch_size, validate, training_set, validation_set, shuffle_training_set, shuffle_validation_set):
+	# creating DataLoaders
+	training_loader = DataLoader(dataset=training_set, batch_size=batch_size, shuffle=shuffle_training_set, collate_fn=collate_raster_vector)
+	if validate:
+		validation_loader = DataLoader(dataset=validation_set, batch_size=batch_size, shuffle=shuffle_validation_set, collate_fn=collate_raster_vector)
+
+	total_samples = len(training_set)
+	n_iterations = math.ceil(total_samples/batch_size)
+	    
+	# saving the losses from every epoch
+	training_losses = []
+	validation_losses = []
+
+	start_time = time.perf_counter()
+	    
+	for epoch in range(n_epochs):
+		# tracking loss per epoch
+		training_running_loss = 0.0
+		n_training_batches = 0
+		validation_running_loss = 0.0
+		n_validation_batches = 0
+
+		# training phase
+		model.train()
+
+		for i, (raster, vector, operators) in enumerate(training_loader):
+			n_training_batches += 1
+	        
+			# moving the features to device
+			raster = raster.to(device)
+			vector = vector.to(device)
+			operators = operators.to(device)
+	    
+			# empty the gradients
+			optimizer.zero_grad()
+	            
+			# forward pass
+			pred_operators_logits = model(raster, vector) # compute predictions, calls forward method under the hood
+			loss = criterion(pred_operators_logits, operators) # calculate loss
+			training_running_loss += loss.item() # tracking running loss to keep track of the loss for every epoch
+	    
+			# backward pass
+			loss.backward() # backpropagation
+			optimizer.step() # update the parameters
+	    
+			# print information every few batches
+			if not (i + 1) % (n_iterations // 10):
+				print(f"epoch {epoch+1}/{n_epochs}, step {i+1}/{n_iterations}")
+
+		training_loss_epoch = training_running_loss / n_training_batches
+		training_losses.append(training_loss_epoch)
+
+		if validate:
+			# validation phase
+			model.eval()
+
+			with torch.no_grad():
+				for raster, vector, operators in validation_loader:
+					n_validation_batches += 1
+		            
+					# moving the features to device
+					raster = raster.to(device)
+					vector = vector.to(device)
+					operators = operators.to(device)
+
+					# prediction on the trained model results in logits
+					pred_operators_logits = model(raster, vector)
+					# calculate and store validation loss
+					loss = criterion(pred_operators_logits, operators)
+					validation_running_loss += loss.item()
+		    
+			validation_loss_epoch = validation_running_loss / n_validation_batches
+			validation_losses.append(validation_loss_epoch)
+	    
+			print(f"epoch {epoch+1} finished, training loss: {training_loss_epoch:.3f}, validation loss: {validation_loss_epoch:.3f}")
+		else:
+			validation_losses.append(0)
+			print(f"epoch {epoch+1} finished, training loss: {training_loss_epoch:.3f}")
+
+	end_time = time.perf_counter()
+	training_time = end_time - start_time
+	print(f"Training time: {training_time:,.3f} seconds")
+
+	return model, training_losses, validation_losses, training_time
