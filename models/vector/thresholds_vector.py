5c330b0129e759bf7f7c11dce52501bf723a3a84
jorissenn <sennjoris@gmail.com>
2024-10-08 16:44:42 +0200

    implemented optimal thresholds for vector model

---
+++ models/vector/thresholds_vector.py
@@ -0,0 +1,16 @@
+vector_thresholds = {
+	"HGNN elimination": 0.45,
+	"HGNN selection": {
+		"aggregation": 0.45, 
+		"typification": 0.35, 
+		"displacement": 0.5, 
+		"enlargement": 0.4
+	},
+	"HGT elimination": 0.45,
+	"HGT selection": {
+		"aggregation": 0.5, 
+		"typification": 0.3, 
+		"displacement": 0.5, 
+		"enlargement": 0.5
+	}
+}
