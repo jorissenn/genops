654942fcdb083f5312a0ffe5bab4b5bbe00186a4
jorissenn <sennjoris@gmail.com>
2024-10-15 14:01:01 +0200

    recomputed accuracy metrics for unimodal models

--- models/raster/thresholds_raster.py
+++ models/raster/thresholds_raster.py
@@ -1,16 +1,16 @@
 raster_thresholds = {
-	"CNN elimination": 0.5,
+	"CNN elimination": 0.45,
 	"CNN selection": {
 		"aggregation": 0.55, 
 		"typification": 0.45, 
 		"displacement": 0.45, 
 		"enlargement": 0.5
 	},
-	"ViT elimination": 0.5,
+	"ViT elimination": 0.45,
 	"ViT selection": {
-		"aggregation": 0.5, 
-		"typification": 0.5, 
+		"aggregation": 0.55, 
+		"typification": 0.4, 
 		"displacement": 0.5, 
-		"enlargement": 0.5
+		"enlargement": 0.45
 	}
 }
--- models/vector/thresholds_vector.py
+++ models/vector/thresholds_vector.py
@@ -2,15 +2,15 @@ vector_thresholds = {
 	"HGNN elimination": 0.45,
 	"HGNN selection": {
 		"aggregation": 0.45, 
-		"typification": 0.35, 
-		"displacement": 0.5, 
+		"typification": 0.4, 
+		"displacement": 0.55, 
 		"enlargement": 0.4
 	},
 	"HGT elimination": 0.45,
 	"HGT selection": {
-		"aggregation": 0.5, 
-		"typification": 0.3, 
-		"displacement": 0.5, 
+		"aggregation": 0.55, 
+		"typification": 0.4, 
+		"displacement": 0.55, 
 		"enlargement": 0.5
 	}
 }
