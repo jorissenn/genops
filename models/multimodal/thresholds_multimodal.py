fa729c5845d22cf0f69314dc3cb523b8bf788d2e
jorissenn <sennjoris@gmail.com>
2024-10-29 23:31:35 +0100

    evaluated multimodal model

--- models/multimodal/thresholds_multimodal.py
+++ models/multimodal/thresholds_multimodal.py
@@ -1,8 +1,8 @@
 multimodal_thresholds = {
-	"CNN+HGNN elimination": 0.5,
+	"CNN+HGNN elimination": 0.4,
 	"CNN+HGNN selection": {
 		"aggregation": 0.5, 
-		"typification": 0.5, 
+		"typification": 0.4, 
 		"displacement": 0.5, 
 		"enlargement": 0.5
 	}
